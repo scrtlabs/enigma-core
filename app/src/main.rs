d71a5108ce6be5d6c4934cc3ea40b2b8fdf8daee
Isan-Rivkin <isanrivkin@gmail.com>
2018-06-10 17:14:23 +0300

    MAIN: cleaned main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -17,27 +17,18 @@ fn main() {
 
     /* this is an example of initiating an enclave */
 
-    let enclave = match esgx::general::init_enclave() {
-        Ok(r) => {
-            println!("[+] Init Enclave Successful {}!", r.geteid());
-            r
-        },
-        Err(x) => {
-            println!("[-] Init Enclave Failed {}!", x.as_str());
-            return;
-        },
-    };
-    
-    // quote test 
-    // let spid = String::from("3DDB338BD52EE314B01F1E4E1E84E8AA");
-    // let mut encoded = esgx::equote::produce_quote(&enclave, &spid);
-    // println!("{}",encoded );
-
-    // sealing test 
-    // ret = unsafe {
-    //     esgx::estorage::ecall_test_sealing_storage_key(enclave.geteid(), &mut ret)
+    // let enclave = match esgx::general::init_enclave() {
+    //     Ok(r) => {
+    //         println!("[+] Init Enclave Successful {}!", r.geteid());
+    //         r
+    //     },
+    //     Err(x) => {
+    //         println!("[-] Init Enclave Failed {}!", x.as_str());
+    //         return;
+    //     },
     // };
-    enclave.destroy();
+
+    //    enclave.destroy();
 }
 
 #[cfg(test)]
@@ -49,3 +40,6 @@ mod tests {
     }
 
 }
+
+
+
