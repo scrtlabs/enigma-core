b19ae67f5ae41f744cfc0d67bb30bf79e8ff0e8b
Frederic Fortier <fredfortier@gmail.com>
2018-06-15 18:36:42 -0700

    BLD: working on the register function

--- ethbridge/src/lib.rs
+++ ethbridge/src/lib.rs
@@ -1,6 +1,11 @@
 extern crate web3;
 extern crate rustc_hex;
+extern crate mockers;
+extern crate serde_json;
 
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 use web3::Web3;
 use web3::transports::Http;
 use web3::types::Address;
@@ -9,23 +14,29 @@ use std::thread;
 use web3::futures::Future;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use web3::contract::Contract;
+use serde_json::{Value, Error};
 
+/// Trying to make this an abstraction so the implementation can be externalized but
+/// I'm not having any luck passing a Trait to Arc
 pub trait Emittable
 {
     fn new() -> Self;
     fn emit_epoch(&self, block: usize);
 }
 
-pub struct EpochGenerator {}
+pub struct EpochGen {}
 
-impl Emittable for EpochGenerator {
+impl Emittable for EpochGen
+{
     fn new() -> Self
     {
-        EpochGenerator {}
+        EpochGen {}
     }
+
     fn emit_epoch(&self, block: usize)
     {
-        println!("registering new epoch for block: {:?}", block);
+        println!("emitting epoch: {}", block);
     }
 }
 
@@ -34,6 +45,7 @@ pub struct Ledger
     w3: Web3<Http>,
 }
 
+/// For operations on the Ethereum ledger
 impl Ledger
 {
     pub fn new(w3: Web3<Http>) -> Self
@@ -41,7 +53,7 @@ impl Ledger
         Ledger { w3 }
     }
 
-    pub fn watch_blocks(&self, eloop: web3::transports::EventLoopHandle, epoch_generator: Arc<EpochGenerator>, max_rounds: usize)
+    pub fn watch_blocks(&self, eloop: web3::transports::EventLoopHandle, epoch_generator: Arc<EpochGen>, max_rounds: usize)
     {
         let blocks_in_epoch: usize = 5;
         let block = Arc::new(AtomicUsize::new(0));
@@ -65,9 +77,7 @@ impl Ledger
                 Ok(())
             });
 
-            eloop.remote().spawn(|_| {
-                f
-            });
+            eloop.remote().spawn(|_| f);
             thread::sleep(time::Duration::from_secs(10));
         }
     }
@@ -76,15 +86,46 @@ impl Ledger
 pub struct EnigmaContract
 {
     w3: Web3<Http>,
-    address: Address,
+    contract: Contract<Http>,
 }
 
+/// For operations on the Enigma contract deployed on Ethereum
 impl EnigmaContract
 {
-    pub fn new(w3: Web3<Http>, address: Address) -> Self
+    pub fn new(w3: Web3<Http>, address: &str, path: &str) -> Self
     {
-        EnigmaContract { w3: w3, address: address }
+        let contract = EnigmaContract::deployed(&w3, address, path);
+        EnigmaContract { w3, contract }
     }
+
+    /// Fetch the Enigma contract deployed on Ethereum using an HTTP Web3 provider
+    fn deployed(w3: &Web3<Http>, address: &str, path: &str) -> Contract<Http>
+    {
+        let mut f = File::open(path)
+            .expect("file not found");
+
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+
+        let contract_address: Address = address
+            .parse()
+            .unwrap();
+
+        let v: Value = serde_json::from_str(&contents).unwrap();
+        let abi = serde_json::to_string(&v["abi"]).unwrap();
+        println!("fetching contract deployed at {:?}, with abi: {}", contract_address, abi);
+
+        let contract = Contract::from_json(
+            w3.eth(),
+            contract_address,
+            abi.as_bytes(),
+        ).unwrap();
+
+        contract
+    }
+
+    /// Call the register transactional method of the Enigma contract
     pub fn register(&self) -> String
     {
         String::from("register")
@@ -95,6 +136,7 @@ impl EnigmaContract
 #[cfg(test)]
 mod tests {
     use super::*;
+    use mockers::Scenario;
 
     fn setup() -> (web3::transports::EventLoopHandle, Web3<Http>)
     {
@@ -107,10 +149,8 @@ mod tests {
     fn it_registers()
     {
         let (_, w3) = setup();
-        let contract_address: Address = "eec918d74c746167564401103096d45bbd494b74"
-            .parse()
-            .unwrap();
-        let contract = EnigmaContract::new(w3, contract_address);
+
+        let contract = EnigmaContract::new(w3, "eec918d74c746167564401103096d45bbd494b74", "/Users/fredfortier/Code/enigma/mvp0/coin-mixer-poc/dapp/build/contracts/Enigma.json");
         let tx = contract.register();
         assert!(tx.len() > 0);
     }
@@ -120,8 +160,9 @@ mod tests {
     {
         let (eloop, w3) = setup();
         let ledger = Ledger::new(w3);
-        let eg: EpochGenerator = EpochGenerator::new();
+        let eg = EpochGen::new();
+
         let epoch_generator = Arc::new(eg);
-//        ledger.watch_blocks(eloop, epoch_generator, 3);
+        ledger.watch_blocks(eloop, epoch_generator, 3);
     }
 }
