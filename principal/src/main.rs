131f1a29b4333c2ced0272224f86851dad555a6d
Frederic Fortier <fredfortier@gmail.com>
2018-06-15 20:59:35 -0700

    BLD: housekeeping, all tests passing

--- principal/src/main.rs
+++ principal/src/main.rs
@@ -1,3 +1,34 @@
+extern crate principal;
+extern crate web3;
+
+use web3::Web3;
+use principal::Ledger;
+use principal::EpochGen;
+use std::sync::Arc;
+
+trait Emittable {
+    fn new() -> Self;
+    fn emit_epoch(&self, block: usize);
+}
+
+impl Emittable for EpochGen {
+    fn new() -> Self {
+        EpochGen {}
+    }
+
+    fn emit_epoch(&self, block: usize) {
+        println!("emitting epoch: {}", block);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (eloop, http) = web3::transports::Http::new("http://localhost:9545")
+        .expect("unable to create Web3 HTTP provider");
+    let w3 = web3::Web3::new(http);
+
+    let ledger = Ledger::new(w3);
+    let eg = EpochGen::new();
+
+    let epoch_generator = Arc::new(eg);
+    ledger.watch_blocks(eloop, epoch_generator, 3);
 }
