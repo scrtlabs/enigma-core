0484ead8b9e723732715c120bdadfbe48fcd930e
Frederic Fortier <fredfortier@gmail.com>
2018-06-18 14:27:14 -0700

    BLD: housekeeping

--- principal/src/lib.rs
+++ principal/src/lib.rs
@@ -10,6 +10,8 @@ use web3::futures::Future;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+pub struct EpochGen {}
+
 /// Trying to make this an abstraction so the implementation can be externalized but
 /// I'm not having any luck passing a Trait to Arc
 trait Emittable
@@ -17,22 +19,19 @@ trait Emittable
     fn new() -> Self;
     fn emit_epoch(&self, block: usize);
 }
-pub struct EpochGen {}
+
 impl Emittable for EpochGen
 {
-    fn new() -> Self
-    {
+    fn new() -> Self {
         EpochGen {}
     }
 
-    fn emit_epoch(&self, block: usize)
-    {
+    fn emit_epoch(&self, block: usize) {
         println!("emitting epoch: {}", block);
     }
 }
 
-pub struct Ledger
-{
+pub struct Ledger {
     w3: Web3<Http>,
 }
 
@@ -44,8 +43,7 @@ impl Ledger
         Ledger { w3 }
     }
 
-    pub fn watch_blocks(&self, eloop: web3::transports::EventLoopHandle, epoch_generator: Arc<EpochGen>, max_rounds: usize)
-    {
+    pub fn watch_blocks(&self, eloop: web3::transports::EventLoopHandle, epoch_generator: Arc<EpochGen>, max_rounds: usize) {
         let blocks_in_epoch: usize = 5;
         let block = Arc::new(AtomicUsize::new(0));
         for _ in 0..max_rounds {
@@ -78,8 +76,7 @@ impl Ledger
 mod tests {
     use super::*;
 
-    fn setup() -> (web3::transports::EventLoopHandle, Web3<Http>)
-    {
+    fn setup() -> (web3::transports::EventLoopHandle, Web3<Http>) {
         let (_eloop, http) = web3::transports::Http::new("http://localhost:9545")
             .expect("unable to create Web3 HTTP provider");
         let w3 = web3::Web3::new(http);
@@ -87,8 +84,7 @@ mod tests {
     }
 
     #[test]
-    fn it_watch_blocks()
-    {
+    fn it_watch_blocks() {
         let (eloop, w3) = setup();
         let ledger = Ledger::new(w3);
         let eg = EpochGen::new();
