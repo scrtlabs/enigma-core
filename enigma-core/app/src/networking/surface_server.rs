14537fd76852cf1358976eb40aebdf426d991ff9
Isan-Rivkin <isanrivkin@gmail.com>
2018-06-19 19:49:01 +0300

    MAINT: minor fix

--- enigma-core/app/src/networking/surface_server.rs
+++ enigma-core/app/src/networking/surface_server.rs
@@ -155,11 +155,10 @@ impl<'a> Server<'a>{
     use esgx::general::init_enclave;
     use networking::surface_server;
     use networking::constants;
-
     // can be tested with a client /app/tests/surface_listener/surface_client.pu
     // network message defitnitions can be found in /app/tests/surface_listener/message_type.definition
      #[test]
-     //#[ignore]
+     #[ignore]
      fn test_run_server(){ 
             // initiate the enclave 
             let enclave = match init_enclave() {
