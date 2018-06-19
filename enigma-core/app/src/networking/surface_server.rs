0fb6dd77768e25962f5c6a2b2a1f733352e3adf1
Isan-Rivkin <isanrivkin@gmail.com>
2018-06-19 11:54:46 +0300

    DEV: working on server robusttness

--- enigma-core/app/src/networking/surface_server.rs
+++ enigma-core/app/src/networking/surface_server.rs
@@ -130,7 +130,7 @@ impl<'a> Server<'a>{
                     }
                 },
                 Err(e)=>{
-                    println!("[-] Server Err : {:?}",e);
+                    println!("[-] Server Err {}, {}", e.cause(), e.backtrace());
                 }
             }
         }
@@ -151,7 +151,7 @@ impl<'a> Server<'a>{
     // can be tested with a client /app/tests/surface_listener/surface_client.pu
     // network message defitnitions can be found in /app/tests/surface_listener/message_type.definition
      #[test]
-     #[ignore]
+     //#[ignore]
      fn test_run_server(){ 
             // initiate the enclave 
             let enclave = match init_enclave() {
