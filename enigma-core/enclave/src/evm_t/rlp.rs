6b0ede44c043f3436c87e7465866a1c6e6cff855
moria <moria@enigma.co>
2018-06-28 01:59:55 +0300

    BUG: fix decode of rlp-encoded uint parameter

--- enigma-core/enclave/src/evm_t/rlp.rs
+++ enigma-core/enclave/src/evm_t/rlp.rs
@@ -23,13 +23,7 @@ fn convert_undecrypted_value_to_string(rlp: &UntrustedRlp, type_str: String) -> 
                 v.clone()
             }
         },
-        Err(_e) => {
-            let num_result: Result<u32, DecoderError> = rlp.as_val();
-            match num_result{
-                Ok(v) => v.to_string(),
-                Err(_e) => "".to_string(),
-            }
-        },
+        Err(_e) =>  "".to_string(),
     };
     result
 }
@@ -44,50 +38,63 @@ pub fn complete_to_u256(num: String) -> String{
 }
 
 fn decrypt_rlp(rlp: &UntrustedRlp, result: & mut String, key: &[u8], type_str: &String) {
-    if rlp.is_list(){
+    if rlp.is_list() {
         result.push_str("[");
         let iter = rlp.iter();
-        for item in iter{
+        for item in iter {
             decrypt_rlp(&item, result, key, type_str);
             result.push_str(",");
         }
         //Replace the last ',' with ']'
         result.pop();
         result.push_str("]");
-    }
-    else{
-        let as_val: Result<Vec<u8>, DecoderError> = rlp.as_val();
-        let value: String = match as_val{
+    } else {
+        let num_result: Result<u64, DecoderError> = rlp.as_val();
+        let value: String = match num_result {
             Ok(v) => {
-                let encrypted_value = read_hex(from_utf8(&v).unwrap()).unwrap();
-                let decrypted_value = decrypt(&encrypted_value,key);
-                match decrypted_value{
-                    Ok(v)=> {
-                        let iter = v.into_iter();
-                        let mut decrypted_str = "".to_string();
-                        for item in iter{
-                            decrypted_str.push(item as char);
-                        }
+                if type_str.starts_with("uint") {
+                    complete_to_u256(v.to_string())
+                }
+                else {
+                    v.to_string()
+                }
+            },
+            Err(_e) => {
+                let as_val: Result<Vec<u8>, DecoderError> = rlp.as_val();
+                let value: String = match as_val {
+                    Ok(v) => {
+                        let encrypted_value = read_hex(from_utf8(&v).unwrap()).unwrap();
+                        let decrypted_value = decrypt(&encrypted_value, key);
+                        match decrypted_value {
+                            Ok(v) => {
+                                let iter = v.into_iter();
+                                let mut decrypted_str = "".to_string();
+                                for item in iter {
+                                    decrypted_str.push(item as char);
+                                }
 
-                        //Remove 0x from the beginning, if used in encryption
-                        if type_str.starts_with("address") & decrypted_str.starts_with("0x") {
-                            decrypted_str.remove(0);
-                            decrypted_str.remove(0);
-                        }
+                                //Remove 0x from the beginning, if used in encryption
+                                if type_str.starts_with("address") & decrypted_str.starts_with("0x") {
+                                    decrypted_str.remove(0);
+                                    decrypted_str.remove(0);
+                                }
 
-                        if type_str.starts_with("uint") {
-                            decrypted_str = complete_to_u256(decrypted_str);
+                                if type_str.starts_with("uint") {
+                                    decrypted_str = complete_to_u256(decrypted_str);
+                                }
+                                decrypted_str
+                            },
+                            Err(_e) => {
+                                convert_undecrypted_value_to_string(rlp, type_str.to_string())
+                            },
                         }
-                        decrypted_str
                     },
                     Err(_e) => {
                         convert_undecrypted_value_to_string(rlp, type_str.to_string())
                     },
-                }
-            },
-            Err(_e) => {
-                convert_undecrypted_value_to_string(rlp, type_str.to_string())
-            },
+                };
+                value
+            }
         };
         result.push_str(&value);
     }
