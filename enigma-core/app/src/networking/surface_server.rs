75f7d96406aad7878061e3d9172b33ce6e97d47a
Isan-Rivkin <isanrivkin@gmail.com>
2018-07-01 19:27:32 +0300

    TST: changed test

--- enigma-core/app/src/networking/surface_server.rs
+++ enigma-core/app/src/networking/surface_server.rs
@@ -166,7 +166,7 @@ impl Server{
     // can be tested with a client /app/tests/surface_listener/surface_client.pu
     // network message defitnitions can be found in /app/tests/surface_listener/message_type.definition
      #[test]
-     //#[ignore]
+     #[ignore]
      fn test_run_server(){ 
             // initiate the enclave 
             let enclave = match init_enclave() {
