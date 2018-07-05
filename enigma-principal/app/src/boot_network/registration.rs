8a30cc710ea13bb9a55fd9fcf92cecb4e0c6ffd0
Isan-Rivkin <isanrivkin@gmail.com>
2018-07-05 21:47:52 +0300

    DEV: registration and gas

--- enigma-principal/app/src/boot_network/registration.rs
+++ enigma-principal/app/src/boot_network/registration.rs
@@ -5,11 +5,10 @@ use enigma_tools_u::attestation_service::constants;
 use failure::Error;
 //web3
 use web3;
-use web3::futures::Future;
+use web3::futures::{Future, Stream};
 use web3::contract::{Contract, Options};
-use web3::types::{Address, U256};
+use web3::types::{Address, U256, Bytes};
 use rustc_hex::FromHex;
-
 
 // encoding in surface https://github.com/enigmampc/surface/blob/e179790347e03666ad24829545429bcb69867849/src/surface/communication/core/worker.py#L105
 
@@ -33,6 +32,37 @@ pub fn web3_test(){
     let accounts = web3.eth().accounts().wait().unwrap();
     println!("Accounts: {:?}", accounts);
 }
+
+pub fn register_worker(signer : &String, report : &Vec<u8>){
+    let (_eloop, transport) = web3::transports::Http::new("http://localhost:9545").unwrap();
+    let web3 = web3::Web3::new(transport);
+    let accounts = web3.eth().accounts().wait().unwrap();
+    // load the contract 
+    let eng_address : Address ="345cA3e014Aaf5dcA488057592ee47305D9B3e10".parse().unwrap();
+    let contract = Contract::from_json(web3.eth(), eng_address, include_bytes!("./enigma.abi"),).unwrap();
+    // register 
+    let signer_addr : Address = signer.parse().unwrap();
+    let byte_report : Bytes = Bytes(report.to_vec());
+    //contract.call("test",(11,),accounts[0], Options::default());
+    
+    
+    //contract.call("register",(signer_addr,report.to_vec(),12,),accounts[0], Options::default());//.
+    // then(|tx| {
+    //     println!("got tx: {:?}", tx);
+    //     Ok(())
+    // });
+    let small : Vec<u8> = [report[0],report[1]].to_vec();
+    let mut options = Options::default();
+    let mut gas : U256 = U256::from_dec_str("1000000").unwrap();
+    println!("send with gas = {:?}",gas );
+    options.gas = Some(gas);
+    contract.call("register",(signer_addr,report.to_vec(),1,),accounts[0],options ).wait().unwrap();
+    let res = contract.query("test_view",(),None,Options::default(),None);
+    let num : U256 = res.wait().unwrap();
+    println!("result from call = {:?}",num );
+    // confirm registration 
+}
+
 pub fn run(){
     let as_response = get_report().unwrap();
     // certificate,signature,report_string are all need to be rlp encoded and send to register() func in enigma contract
@@ -42,5 +72,6 @@ pub fn run(){
     // rlp encoding 
     let encoded : Vec<u8> = rlp_encode_registration_params(&certificate, &signature, &report_string);
     // register worker 
-    web3_test();
+    let signer_addr = String::from("a8d18dbf9d6876fb8bc4dc485b8e0a3d86908650");
+    register_worker(&signer_addr, &encoded);
 }
