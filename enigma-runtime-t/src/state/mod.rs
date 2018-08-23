4700749b62db7b5e8f507aeb7698320ded7645c5
Elichai Turkel <elichai.turkel@gmail.com>
2018-08-23 18:21:34 +0300

    TST: Added tests for the State struct

--- enigma-runtime-t/src/state/mod.rs
+++ enigma-runtime-t/src/state/mod.rs
@@ -54,12 +54,63 @@ impl ContractStateInterface for ContractState {
         Ok(buf)
     }
 
-    fn read_key(&self, key: &str) -> Value {
-        self[key]
+}
+
+
+pub mod tests {
+    use state::*;
+    use std::string::ToString;
+    use serde_json::{Value, Map};
+
+    pub fn test_serialization() {
+        let con = ContractState {
+            contract_id: "Enigma".to_string(),
+            json: json!({"widget":{"debug":"on","window":{"title":"Sample Konfabulator Widget","name":"main_window","width":500,"height":500},"image":{"src":"Images/Sun.png","name":"sun1","hOffset":250,"vOffset":250,"alignment":"center"},"text":{"data":"Click Here","size":36,"style":"bold","name":"text1","hOffset":250,"vOffset":100,"alignment":"center","onMouseUp":"sun1.opacity = (sun1.opacity / 100) * 90;"}}})
+        };
+        assert_eq!(con.serialize().unwrap(), vec![129, 166, 119, 105, 100, 103, 101, 116, 132, 165, 100, 101, 98, 117, 103, 162, 111, 110, 165, 105, 109, 97, 103, 101, 133, 169, 97, 108, 105, 103, 110, 109, 101, 110, 116, 166, 99, 101, 110, 116, 101, 114, 167, 104, 79, 102, 102, 115, 101, 116, 204, 250, 164, 110, 97, 109, 101, 164, 115, 117, 110, 49, 163, 115, 114, 99, 174, 73, 109, 97, 103, 101, 115, 47, 83, 117, 110, 46, 112, 110, 103, 167, 118, 79, 102, 102, 115, 101, 116, 204, 250, 164, 116, 101, 120, 116, 136, 169, 97, 108, 105, 103, 110, 109, 101, 110, 116, 166, 99, 101, 110, 116, 101, 114, 164, 100, 97, 116, 97, 170, 67, 108, 105, 99, 107, 32, 72, 101, 114, 101, 167, 104, 79, 102, 102, 115, 101, 116, 204, 250, 164, 110, 97, 109, 101, 165, 116, 101, 120, 116, 49, 169, 111, 110, 77, 111, 117, 115, 101, 85, 112, 217, 41, 115, 117, 110, 49, 46, 111, 112, 97, 99, 105, 116, 121, 32, 61, 32, 40, 115, 117, 110, 49, 46, 111, 112, 97, 99, 105, 116, 121, 32, 47, 32, 49, 48, 48, 41, 32, 42, 32, 57, 48, 59, 164, 115, 105, 122, 101, 36, 165, 115, 116, 121, 108, 101, 164, 98, 111, 108, 100, 167, 118, 79, 102, 102, 115, 101, 116, 100, 166, 119, 105, 110, 100, 111, 119, 132, 166, 104, 101, 105, 103, 104, 116, 205, 1, 244, 164, 110, 97, 109, 101, 171, 109, 97, 105, 110, 95, 119, 105, 110, 100, 111, 119, 165, 116, 105, 116, 108, 101, 186, 83, 97, 109, 112, 108, 101, 32, 75, 111, 110, 102, 97, 98, 117, 108, 97, 116, 111, 114, 32, 87, 105, 100, 103, 101, 116, 165, 119, 105, 100, 116, 104, 205, 1, 244]);
     }
 
-    fn write_key(&mut self, key: &str, value: Value) -> Option {
-        self[key] = value;
-        None
+    pub fn test_deserialization() {
+        let con = ContractState {
+            contract_id: "Enigma".to_string(),
+            json: json!({"widget":{"debug":"on","window":{"title":"Sample Konfabulator Widget","name":"main_window","width":500,"height":500},"image":{"src":"Images/Sun.png","name":"sun1","hOffset":250,"vOffset":250,"alignment":"center"},"text":{"data":"Click Here","size":36,"style":"bold","name":"text1","hOffset":250,"vOffset":100,"alignment":"center","onMouseUp":"sun1.opacity = (sun1.opacity / 100) * 90;"}}})
+        };
+
+        assert_eq!(con, ContractState::parse("Enigma", vec![129, 166, 119, 105, 100, 103, 101, 116, 132, 165, 100, 101, 98, 117, 103, 162, 111, 110, 165, 105, 109, 97, 103, 101, 133, 169, 97, 108, 105, 103, 110, 109, 101, 110, 116, 166, 99, 101, 110, 116, 101, 114, 167, 104, 79, 102, 102, 115, 101, 116, 204, 250, 164, 110, 97, 109, 101, 164, 115, 117, 110, 49, 163, 115, 114, 99, 174, 73, 109, 97, 103, 101, 115, 47, 83, 117, 110, 46, 112, 110, 103, 167, 118, 79, 102, 102, 115, 101, 116, 204, 250, 164, 116, 101, 120, 116, 136, 169, 97, 108, 105, 103, 110, 109, 101, 110, 116, 166, 99, 101, 110, 116, 101, 114, 164, 100, 97, 116, 97, 170, 67, 108, 105, 99, 107, 32, 72, 101, 114, 101, 167, 104, 79, 102, 102, 115, 101, 116, 204, 250, 164, 110, 97, 109, 101, 165, 116, 101, 120, 116, 49, 169, 111, 110, 77, 111, 117, 115, 101, 85, 112, 217, 41, 115, 117, 110, 49, 46, 111, 112, 97, 99, 105, 116, 121, 32, 61, 32, 40, 115, 117, 110, 49, 46, 111, 112, 97, 99, 105, 116, 121, 32, 47, 32, 49, 48, 48, 41, 32, 42, 32, 57, 48, 59, 164, 115, 105, 122, 101, 36, 165, 115, 116, 121, 108, 101, 164, 98, 111, 108, 100, 167, 118, 79, 102, 102, 115, 101, 116, 100, 166, 119, 105, 110, 100, 111, 119, 132, 166, 104, 101, 105, 103, 104, 116, 205, 1, 244, 164, 110, 97, 109, 101, 171, 109, 97, 105, 110, 95, 119, 105, 110, 100, 111, 119, 165, 116, 105, 116, 108, 101, 186, 83, 97, 109, 112, 108, 101, 32, 75, 111, 110, 102, 97, 98, 117, 108, 97, 116, 111, 114, 32, 87, 105, 100, 103, 101, 116, 165, 119, 105, 100, 116, 104, 205, 1, 244]).unwrap());
+    }
+
+    pub fn test_reserialization() {
+        let con = ContractState {
+            contract_id: "Enigma".to_string(),
+            json: json!({"widget":{"debug":"on","window":{"title":"Sample Konfabulator Widget","name":"main_window","width":500,"height":500},"image":{"src":"Images/Sun.png","name":"sun1","hOffset":250,"vOffset":250,"alignment":"center"},"text":{"data":"Click Here","size":36,"style":"bold","name":"text1","hOffset":250,"vOffset":100,"alignment":"center","onMouseUp":"sun1.opacity = (sun1.opacity / 100) * 90;"}}})
+        };
+        let ser = con.serialize().unwrap();
+        let de = ContractState::parse("Enigma", ser).unwrap();
+
+        assert_eq!(de, con);
+    }
+
+    pub fn test_write() {
+        let mut con = ContractState::new("Enigma");
+        con.write_key("code", json!(200)).unwrap();
+        con.write_key("success", json!(true)).unwrap();
+        con.write_key("payload", json!({ "features": ["serde", "json"] })).unwrap();
+
+        let cmp = ContractState {
+            contract_id: "Enigma".to_string(),
+            json: json!({"code": 200,"success": true,"payload": {"features": ["serde","json"]}})
+        };
+        assert_eq!(con, cmp);
+    }
+
+    pub fn test_read() {
+        let con = ContractState {
+            contract_id: "Enigma".to_string(),
+            json: json!({"code": 200,"success": true,"payload": {"features": ["serde","json"]}})
+        };
+        assert_eq!(con.read_key::<u64>("code").unwrap(), 200);
+        assert_eq!(con.read_key::<bool>("success").unwrap(), true);
+        assert_eq!(con.read_key::<Map<String, Value>>("payload").unwrap()["features"], json!(["serde","json"]));
+
     }
 }
