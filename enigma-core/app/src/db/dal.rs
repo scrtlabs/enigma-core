0acdab3c8ee04836093821a14fc2cee6a0a25cc3
Elichai Turkel <elichai.turkel@gmail.com>
2018-08-27 16:32:46 +0300

    DOC: Documented the CRUD operations of the DAL

--- enigma-core/app/src/db/dal.rs
+++ enigma-core/app/src/db/dal.rs
@@ -15,9 +15,44 @@ pub struct DB<'a> {
 }
 
 pub trait DBInterface {
+    /// Constructs a new `DB<'a>`. with a db file accordingly.
+    ///
+    /// You need to pass a path for the db file
+    /// and a flag to say if this should create the file if missing
+    ///
+    /// This Supports all the CRUD operations
+    /// # Examples
+    /// ```
+    /// let db = DB::new(PathBuf::from("/test/test.db", false);
+    /// ```
     fn new(path: PathBuf, create_if_missing: boolean) -> DB;
-    fn create(&mut self, key: &str, value: &[u8]) -> Result<(), Error>;
+    /// Creates a new Key-Value pair
+    ///
+    /// # Examples
+    /// ```
+    /// db.create("test", "abc".as_bytes()).unwrap();
+    /// ```
+    fn create(&mut self, key: &str, value: &[u8]) -> Result<(), Error>; // TODO: Decide what to do if key doesn't exist
+    /// Reads the Value in a specific Key
+    ///
+    /// # Examples
+    /// ```
+    /// let res = db.read("test").unwrap();
+    /// assert_eq!("abc".as_bytes, res);
+    /// ```
     fn read(&mut self, key: &str) -> Result<Vec<u8>, Error>;
+    /// Updates an existing Key with a new value
+    ///
+    /// # Examples
+    /// ```
+    /// db.update("test", "abc".as_bytes()).unwrap();
+    /// ```
     fn update(&mut self, key: &str, value: &[u8]) -> Result<(), Error>;
+    /// Deletes an existing key
+    ///
+    /// # Examples
+    /// ```
+    /// db.delete("test").unwrap();
+    /// ```
     fn delete(&mut self, key: &str) -> Result<(), Error>;
 }
