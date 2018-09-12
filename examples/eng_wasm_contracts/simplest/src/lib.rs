ec52198e428a40d2ce5f4f994d2bcd2f41f86e99
moria <moria@enigma.co>
2018-09-12 22:43:43 +0300

    DEV: create real basic eng_wasm and example of usage in contract

--- examples/eng_wasm_contracts/simplest/src/lib.rs
+++ examples/eng_wasm_contracts/simplest/src/lib.rs
@@ -1,9 +1,19 @@
 extern crate eng_wasm;
 
+pub struct Error;
+
 #[no_mangle]
-pub fn call() -> i32{
-    eng_wasm::external_function()
+pub fn call() {
+    let key = "code";
+    let value = "157";
+    eng_wasm::write(key, value.as_bytes());
+    let value1 = eng_wasm::read(key);
 }
 
 #[no_mangle]
 pub fn deploy() {}
+
+#[no_mangle]
+pub fn problem() ->Result<i32, Error>{
+    Err(Error)
+}
