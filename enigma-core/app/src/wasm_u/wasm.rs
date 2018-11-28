c50c5957a10a8d47c8c5361eb216f447f3f65b6c
moria <moria@enigma.co>
2018-11-28 11:16:40 +0200

    TST: refactoring wasm tests

--- enigma-core/app/src/wasm_u/wasm.rs
+++ enigma-core/app/src/wasm_u/wasm.rs
@@ -69,7 +69,7 @@ pub fn build_constructor(wasm_code: &[u8]) -> Result<Vec<u8>, Error> {
 
 
 const MAX_EVM_RESULT: usize = 100_000;
-pub fn deploy(eid: sgx_enclave_id_t,  bytecode: &[u8])-> Result<Vec<u8>, Error> {
+pub fn deploy(eid: sgx_enclave_id_t,  bytecode: &[u8])-> Result<Box<[u8]>, Error> {
 
     let deploy_bytecode = build_constructor(&bytecode)?;
     let mut retval: sgx_status_t = sgx_status_t::SGX_SUCCESS;
@@ -84,7 +84,7 @@ pub fn deploy(eid: sgx_enclave_id_t,  bytecode: &[u8])-> Result<Vec<u8>, Error> 
     };
     let box_ptr = output_ptr as *mut Box<[u8]>;
     let part = unsafe { Box::from_raw(box_ptr ) };
-    Ok(part.to_vec())
+    Ok(*part)
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Default)]
@@ -94,7 +94,7 @@ pub struct WasmResult {
     pub delta: ::db::Delta,
 }
 
-pub fn execute(eid: sgx_enclave_id_t,  bytecode: Vec<u8>, callable: &str, args: &str)-> Result<WasmResult,Error>{
+pub fn execute(eid: sgx_enclave_id_t,  bytecode: Box<[u8]>, callable: &str, args: &str)-> Result<WasmResult,Error>{
     let mut retval: sgx_status_t = sgx_status_t::SGX_SUCCESS;
     let mut output = 0u64;
     let mut delta_data_ptr = 0u64;
@@ -149,6 +149,7 @@ pub mod tests {
     use std::str::from_utf8;
     use std::process::Command;
     use std::path::PathBuf;
+    use sgx_types::*;
 
     fn init_enclave() -> SgxEnclave{
         let enclave = match esgx::general::init_enclave_wrapper() {
@@ -163,15 +164,14 @@ pub mod tests {
         enclave
     }
 
-    #[test]
-    fn compile_test_contract() {
+    fn compile_and_deploy_wasm_contract(eid: sgx_enclave_id_t, test_path: &str) -> Box<[u8]>{
         let mut dir = PathBuf::new();
-        dir.push("../../examples/eng_wasm_contracts/simplest");
+        dir.push(test_path);
         let mut output = Command::new("cargo")
             .current_dir(&dir)
             .args(&["build", "--release"])
             .spawn()
-            .expect(&format!("Failed compiling simplest wasm exmaple: {:?}", &dir) );
+            .expect(&format!("Failed compiling wasm contract: {:?}", &dir) );
 
         assert!(output.wait().unwrap().success());
         dir.push("target/wasm32-unknown-unknown/release/contract.wasm");
@@ -180,9 +180,13 @@ pub mod tests {
         let mut wasm_code = Vec::new();
         f.read_to_end(&mut wasm_code).expect("Failed reading the wasm file");
         println!("Bytecode size: {}KB\n", wasm_code.len()/1024);
+        wasm::deploy(eid, &wasm_code).expect("Deploy Failed")
+    }
 
+    #[test]
+    fn simple() {
         let enclave = init_enclave();
-        let contract_code = wasm::deploy(enclave.geteid(), &wasm_code).expect("Deploy Failed");
+        let contract_code = compile_and_deploy_wasm_contract(enclave.geteid(), "../../examples/eng_wasm_contracts/simplest");
 //        let result = wasm::execute(enclave.geteid(),contract_code, "test(uint256,uint256)", "c20102").expect("Execution failed");
         let result = wasm::execute(enclave.geteid(),contract_code, "write()", "").expect("Execution failed");
         enclave.destroy();
@@ -191,24 +195,8 @@ pub mod tests {
 
     #[test]
     fn eth_bridge() {
-        let mut dir = PathBuf::new();
-        dir.push("../../examples/eng_wasm_contracts/contract_with_eth_calls");
-        let mut output = Command::new("cargo")
-            .current_dir(&dir)
-            .args(&["build", "--release"])
-            .spawn()
-            .expect(&format!("Failed compiling simplest wasm exmaple: {:?}", &dir));
-
-        assert!(output.wait().unwrap().success());
-        dir.push("target/wasm32-unknown-unknown/release/contract.wasm");
-
-        let mut f = File::open(&dir).expect(&format!("Can't open the contract.wasm file: {:?}", &dir));
-        let mut wasm_code = Vec::new();
-        f.read_to_end(&mut wasm_code).expect("Failed reading the wasm file");
-        println!("Bytecode size: {}KB\n", wasm_code.len() / 1024);
-
         let enclave = init_enclave();
-        let contract_code = wasm::deploy(enclave.geteid(), &wasm_code).expect("Deploy Failed");
+        let contract_code = compile_and_deploy_wasm_contract(enclave.geteid(), "../../examples/eng_wasm_contracts/contract_with_eth_calls");
         let result = wasm::execute(enclave.geteid(), contract_code, "test()", "").expect("Execution failed");
         enclave.destroy();
     }
