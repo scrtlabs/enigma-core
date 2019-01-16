a87f2492e3401a35b3607816be74ac11b83d39e6
Frederic Fortier <fredfortier@gmail.com>
2019-01-16 09:47:25 -0800

    DEV: Improved my docker startup script

--- dockerfile/run.sh
+++ dockerfile/run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-docker run --net="host" -it --mount src="$(pwd)/../../",target=/root/src,type=bind --device /dev/isgx enigma-core
+SRC=$(pwd)/../
+echo "Checking your SGX driver..."
+ls /dev/isgx >/dev/null 2>1  && echo -e "SGX Driver installed\n" || echo -e "SGX Driver NOT installed\n"
+echo "Running the Docker container..."
+echo -e "Mapping local $SRC to container's /root/src/\n"
+docker run --net="host" -it --mount src="$SRC",target=/root/src,type=bind --device /dev/isgx enigma-core
