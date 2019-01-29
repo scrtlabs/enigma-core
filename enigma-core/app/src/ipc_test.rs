a16e94aeb0f3bb92a5e60f701657a1c9fe62601c
Avishai Weingarten <avishai@enigma.co>
2019-01-29 14:37:36 +0200

    MAINT: cleaned and organised existing ipc tests

--- enigma-core/app/src/ipc_test.rs
+++ enigma-core/app/src/ipc_test.rs
@@ -38,7 +38,7 @@ pub mod tests {
         re.is_match(msg)
     }
 
-    fn send_receive_ipc(msg: &str, port: &'static str) -> Value {
+    fn conn_and_call_ipc(msg: &str, port: &'static str) -> Value {
         let context = zmq::Context::new();
         let requester = context.socket(zmq::REQ).unwrap();
         assert!(requester.connect(&format!("tcp://localhost:{}", port)).is_ok());
@@ -55,34 +55,26 @@ pub mod tests {
         let port = "5555";
         let id = "867";
         let type_req = "GetRegistrationParams";
-
-        let mut msg = json!({"id" : "", "type" : ""});
-        msg["id"] = json!(id);
-        msg["type"] = json!(type_req);
+        let mut msg = json!({"id" : id, "type" : type_req});
 
         run_core(port);
-        let v: Value = send_receive_ipc(&msg.to_string(), port);
+        let v: Value = conn_and_call_ipc(&msg.to_string(), port);
 
         let id_accepted = v["id"].as_str().unwrap();
-        let result_key = v["result"].as_object().unwrap()["signingKey"].as_str().unwrap();
-        let result_rep = v["result"].as_object().unwrap()["report"].as_str().unwrap();
-        let result_sig = v["result"].as_object().unwrap()["signature"].as_str().unwrap();
+        let result_key: String = serde_json::from_value(v["result"]["signingKey"].clone()).unwrap();
+        let result_rep: String = serde_json::from_value(v["result"]["report"].clone()).unwrap();
+        let result_sig: String = serde_json::from_value(v["result"]["signature"].clone()).unwrap();
         let type_res = v["type"].as_str().unwrap();
 
         assert_eq!(id_accepted, id);
         assert_eq!(type_res, type_req);
-        assert!(is_hex(result_key));
-        assert!(is_hex(result_rep));
-        assert!(is_hex(result_sig));
+        assert!(is_hex(&result_key));
+        assert!(is_hex(&result_rep));
+        assert!(is_hex(&result_sig));
     }
 
-    fn get_encryption_key(port: &'static str, id: &str, type_req: &str, user_pub_key: [u8; 64]) -> Value {
-        let mut msg = json!({"id" : "", "type" : "", "userPubKey": ""});
-        msg["id"] = json!(id);
-        msg["type"] = json!(type_req);
-        msg["userPubKey"] = json!(user_pub_key.to_hex());
-
-        send_receive_ipc(&msg.to_string(), port)
+    fn set_encryption_msg(id: &str, type_req: &str, user_pubkey: [u8; 64])-> Value {
+        json!({"id" : id, "type" : type_req, "userPubKey": user_pubkey.to_hex()})
     }
 
     #[test]
@@ -93,7 +85,8 @@ pub mod tests {
         let (_, user_pubkey) = generate_key_pair();
 
         run_core(port);
-        let v: Value = get_encryption_key(port, id, type_req, user_pubkey);
+        let msg = set_encryption_msg(id, type_req, user_pubkey);
+        let v: Value = conn_and_call_ipc(&msg.to_string(), port);
         let id_accepted = v["id"].as_str().unwrap();
         let result_key = v["result"].as_object().unwrap()["workerEncryptionKey"].as_str().unwrap();
         let result_sig = v["result"].as_object().unwrap()["workerSig"].as_str().unwrap();
@@ -106,14 +99,14 @@ pub mod tests {
     }
 
     #[derive(Debug)]
-    pub struct MessagePack {
+    pub struct ParsedMessage {
         prefix: String,
         data: Vec<String>,
         pub_key: Vec<u8>,
         id: [u8; 12],
     }
 
-    impl MessagePack {
+    impl ParsedMessage {
         pub fn from_value(msg: Value) -> Self {
             let prefix_bytes: Vec<u8> = serde_json::from_value(msg["prefix"].clone()).unwrap();
             let prefix: String = std::str::from_utf8(&prefix_bytes[..]).unwrap().to_string();
@@ -130,20 +123,15 @@ pub mod tests {
         }
     }
 
-    fn get_ptt_req(port: &'static str, id: &str, type_req: &str, addrs: Vec<String>) -> Value {
-        let mut msg = json!({"id" : "", "type" : "", "addresses": ""});
-        msg["id"] = json!(id);
-        msg["type"] = json!(type_req);
-        msg["addresses"] = json!(addrs);
-        send_receive_ipc(&msg.to_string(), port)
-    }
-
-    fn get_packed_msg(val: Value) -> Value {
-        let msg = val["result"].as_object().unwrap()["request"].as_str().unwrap();
+    fn parse_packed_msg(msg: &str) -> Value {
         let msg_bytes = msg.from_hex().unwrap();
         let mut de = Deserializer::new(&msg_bytes[..]);
         Deserialize::deserialize(&mut Deserializer::new(&msg_bytes[..])).unwrap()
 
+    }
+
+    fn set_ptt_req_msg(id: &str, type_req: &str, addrs: Vec<String>) -> Value {
+        json!({"id" : id, "type" : type_req, "addresses": addrs})
     }
 
     #[test]
@@ -154,22 +142,24 @@ pub mod tests {
         let addresses: Vec<String> = vec![generate_address().to_hex(), generate_address().to_hex()];
 
         run_core(port);
-        let v: Value = get_ptt_req(port, id, type_req,addresses.clone());
+        let msg = set_ptt_req_msg(id, type_req,addresses.clone());
+        let v: Value = conn_and_call_ipc(&msg.to_string(), port);
+
         let id_accepted = v["id"].as_str().unwrap();
-        let result_msg = v["result"].as_object().unwrap()["request"].as_str().unwrap();
+        let packed_msg = v["result"].as_object().unwrap()["request"].as_str().unwrap();
         let result_sig = v["result"].as_object().unwrap()["workerSig"].as_str().unwrap();
         let type_res = v["type"].as_str().unwrap();
-        let msg_pack: MessagePack = MessagePack::from_value(get_packed_msg(v.clone()));
+        let unpacked_msg: ParsedMessage = ParsedMessage::from_value(parse_packed_msg(packed_msg));
 
         assert_eq!(id_accepted, id);
         assert_eq!(type_res, type_req);
-        assert_eq!(addresses.len(), msg_pack.data.len());
-        assert_eq!(msg_pack.pub_key.len(), 64);
+        assert_eq!(addresses.len(), unpacked_msg.data.len());
+        assert_eq!(unpacked_msg.pub_key.len(), 64);
         assert!(is_hex(result_sig));
     }
 
-    fn create_principal_response(val: Value) -> Vec<u8> {
-        let unpacked_msg: Value = get_packed_msg(val);
+    fn mock_principal_res(msg: &str) -> Vec<u8> {
+        let unpacked_msg: Value = parse_packed_msg(msg);
         let enc_response: Value = make_encrypted_response(unpacked_msg);
 
         let mut serialized_enc_response = Vec::new();
@@ -177,16 +167,22 @@ pub mod tests {
         serialized_enc_response
     }
 
+    fn set_ptt_res_msg(id: &str, type_res: &str, response: Vec<u8>) -> Value {
+        json!({"id" : id, "type" : type_res, "response": response.to_hex()})
+    }
+
     fn run_ptt_round(port: &'static str, addrs: Vec<String>, id_res: &str, type_res: &str) -> Value {
         let id_req = "536";
         let type_req = "GetPTTRequest";
 
-        let req_val: Value = get_ptt_req(port, id_req, type_req,addrs.clone());
+        // set encrypted request message to send to the principal node
+        let msg_req = set_ptt_req_msg(id_req, type_req,addrs.clone());
+        let req_val: Value = conn_and_call_ipc(&msg_req.to_string(), port);
+        let packed_msg = req_val["result"].as_object().unwrap()["request"].as_str().unwrap();
 
-
-        let enc_response = create_principal_response(req_val);
-        let mut msg = json!({"id": id_res, "type": type_res, "response": enc_response.to_hex()});
-        send_receive_ipc(&msg.to_string(), port)
+        let enc_response = mock_principal_res(packed_msg);
+        let msg = set_ptt_res_msg(id_res, type_res, enc_response);
+        conn_and_call_ipc(&msg.to_string(), port)
     }
 
     #[test]
@@ -206,53 +202,61 @@ pub mod tests {
         assert_eq!(result.len(), 0);
     }
 
-    fn create_shared_from_enckey(v: Value, user_priv: SecretKey) -> Vec<u8> {
-        let result_key = v["result"].as_object().unwrap()["workerEncryptionKey"].as_str().unwrap();
-        let key_slice = result_key.from_hex().unwrap();
-        get_shared_key(&key_slice, user_priv)
+    fn produce_shared_key(port: &'static str) -> (Vec<u8>, Vec<u8>) {
+        // get core's pubkey
+        let id_enc = "7698";
+        let type_enc =  "NewTaskEncryptionKey";
+        let (user_privkey, user_pubkey) = generate_key_pair();
+        let msg = set_encryption_msg(id_enc, type_enc, user_pubkey);
+
+        let v: Value = conn_and_call_ipc(&msg.to_string(), port);
+        let core_pubkey: String = serde_json::from_value(v["result"]["workerEncryptionKey"].clone()).unwrap();
+        let pubkey_slice: Vec<u8> = core_pubkey.from_hex().unwrap();
+        (get_shared_key(&pubkey_slice, user_privkey), user_pubkey.to_vec())
     }
+
+    fn set_deploy_msg(id: &str, type_dep: &str, pre_code: &str, args: &str, callable: &str, usr_pubkey: &str, gas_limit: u64, addr: &str) -> Value {
+        json!({"id" : id, "type" : type_dep, "input":
+                        {"preCode": &pre_code, "encryptedArgs": args,
+                        "encryptedFn": callable, "userPubKey": usr_pubkey,
+                        "gasLimit": gas_limit, "contractAddress": addr}
+                        })
+    }
+
     #[test]
     fn test_deploy_secret_contract() {
         let port =  "5557";
         run_core(port);
 
-        let (user_privkey, user_pubkey) = generate_key_pair();
-        let id_enc = "7698";
-        let type_enc =  "NewTaskEncryptionKey";
-        let enc_res: Value = get_encryption_key(port, id_enc, type_enc, user_pubkey);
-
-        let id_dep = "7699";
-        let type_dep = "DeploySecretContract";
-        let pre_code = get_bytecode_from_path("../../examples/eng_wasm_contracts/simplest");
-        let shared_key: Vec<u8> = create_shared_from_enckey(enc_res, user_privkey);
-        // args
-        let fn_deploy = "construct(uint)";
-        let args_deploy = [Token::Uint(17.into())];
-        let (encrypted_fn, encrypted_args) = serial_and_encrypt_input(&shared_key, fn_deploy, &args_deploy, None);
-        let gas_limit = 100_000_000;
+        // address generation and ptt
         let address = generate_address();
-
         let id_ptt = "876";
         let type_ptt = "PTTResponse";
         let _ = run_ptt_round(port, vec![address.to_hex()], id_ptt, type_ptt);
 
-        let msg = json!({"id" : id_dep, "type" : type_dep, "input":
-                        {"preCode": &pre_code.to_hex(), "encryptedArgs": encrypted_args.to_hex(),
-                        "encryptedFn": encrypted_fn.to_hex(), "userPubKey": user_pubkey.to_hex(),
-                        "gasLimit": gas_limit, "contractAddress": address.to_hex()}
-                        });
-        let v: Value = send_receive_ipc(&msg.to_string(), port);
-        let id_accepted = v["id"].as_str().unwrap();
+        // WUKE- get the arguments encryption key
+        let (shared_key, user_pubkey) = produce_shared_key(port);
+
+        let id_dep = "7699";
+        let type_dep = "DeploySecretContract";
+        let pre_code = get_bytecode_from_path("../../examples/eng_wasm_contracts/simplest");
+        let fn_deploy = "construct(uint)";
+        let args_deploy = [Token::Uint(17.into())];
+        let (encrypted_fn, encrypted_args) = serial_and_encrypt_input(&shared_key, fn_deploy, &args_deploy, None);
+        let gas_limit = 100_000_000;
+
+        let msg = set_deploy_msg(id_dep, type_dep, &pre_code.to_hex(), &encrypted_args.to_hex(),
+                                 &encrypted_fn.to_hex(), &user_pubkey.to_hex(), gas_limit, &address.to_hex());
+        let v: Value = conn_and_call_ipc(&msg.to_string(), port);
+        let accepted_id = v["id"].as_str().unwrap();
         let result_output = v["result"].as_object().unwrap()["output"].as_str().unwrap();
         let result_pre_hash = v["result"].as_object().unwrap()["preCodeHash"].as_str().unwrap();
-        let result_output = v["result"].as_object().unwrap()["delta"].as_object().unwrap();
-        let result_used_gas: u64 = serde_json::from_value(v["result"]["usedGas"].clone()).unwrap();
+        let accepted_delta = v["result"].as_object().unwrap()["delta"].as_object().unwrap();
+        let accepted_used_gas: u64 = serde_json::from_value(v["result"]["usedGas"].clone()).unwrap();
         let type_res = v["type"].as_str().unwrap();
 
-//        assert_eq!(id_accepted, id);
-//        assert_eq!(type_res, type_req);
-//        assert!(is_hex(result_key));
-//        assert!(is_hex(result_sig));
-
+        assert_eq!(id_dep, accepted_id);
+        assert_eq!(type_dep, type_res);
+        assert!(accepted_used_gas > 0);
     }
 }
