7d80bc817ec3c73d9a873cec03179491ce7ecf6e
moria <moria@enigma.co>
2019-02-14 12:48:22 +0200

    TST: fixing of surface tests according to b560949a85028cc934c4c5318eb41412ab9ead41

--- examples/eng_wasm_contracts/simplest/src/lib.rs
+++ examples/eng_wasm_contracts/simplest/src/lib.rs
@@ -10,14 +10,14 @@ use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
 
 use eng_wasm::String;
-use hex::ToHex;
+use hex::{ToHex, FromHex};
 
 #[pub_interface]
 pub trait ContractInterface{
-    fn write();
-    fn check_address(addr: H256);
-    fn check_addresses(addr1: H256, addr2: H256);
-    fn choose_rand_color();
+    fn write() -> Vec<u8>;
+    fn check_address(addr: H256) -> H256;
+    fn check_addresses(addr1: H256, addr2: H256) -> Vec<H256>;
+    fn choose_rand_color() -> Vec<u8>;
     fn get_scrambled_vec();
     fn print_test(x: U256, y: U256);
     fn construct(param: U256);
@@ -49,7 +49,7 @@ impl ContractInterface for Contract {
     /// As a temporary solution the value is converted to a stream of bytes.
     /// Later as part of runtime there will be created a macros for writing and reading any type.
     #[no_mangle]
-    fn write() {
+    fn write() -> Vec<u8> {
         let mut a = String::new();
         a.push_str("157");
         let key = "code";
@@ -57,19 +57,21 @@ impl ContractInterface for Contract {
         let read_val: String = read_state!(key).unwrap();
 
         assert_eq!(read_val, a);
+        read_val.as_bytes().to_vec()
     }
 
     #[no_mangle]
-    fn check_address(addr: H256) {
-
+    fn check_address(addr: H256) -> H256{
         write_state!("addr" => addr.to_hex());
         let read_val: String = read_state!("addr").unwrap();
 
         assert_eq!(read_val, addr.to_hex());
+
+        H256::from_slice(&read_val.from_hex().unwrap())
     }
 
     #[no_mangle]
-    fn check_addresses(addr1: H256, addr2: H256) {
+    fn check_addresses(addr1: H256, addr2: H256) -> Vec<H256> {
         write_state!("addr1" => addr1.to_hex());
         write_state!("addr2" => addr2.to_hex());
 
@@ -78,11 +80,15 @@ impl ContractInterface for Contract {
 
         assert_eq!(read_addr1, addr1.to_hex());
         assert_eq!(read_addr2, addr2.to_hex());
+        let mut ret = Vec::with_capacity(2);
+        ret.push(H256::from_slice(&read_addr1.from_hex().unwrap()));
+        ret.push(H256::from_slice(&read_addr2.from_hex().unwrap()));
+        ret
     }
 
     // tests the random service
     #[no_mangle]
-    fn choose_rand_color() {
+    fn choose_rand_color() -> Vec<u8>{
         let mut colors = Vec::new();
         colors.extend(["green", "yellow", "red", "blue", "white", "black", "orange", "purple"].iter().cloned());
         let random: u8 = Rand::gen();
@@ -90,6 +96,7 @@ impl ContractInterface for Contract {
         let rng_rand = (random as usize) % colors.len();
         write_state!("color" => colors[rng_rand]);
         let color : String = read_state!("color").unwrap();
+        color.as_bytes().to_vec()
     }
 
     // tests the shuffle service on a simple array
@@ -103,9 +110,6 @@ impl ContractInterface for Contract {
     #[no_mangle]
     fn print_test(x: U256, y: U256) {
         eprint!("{:?} {:?}", x.as_u64(), y.as_u64());
-        write_state!("x" => x.as_u64(), "y" => y.as_u64());
-        let x: u64 = read_state!("x").unwrap();
-        let y: u64 = read_state!("y").unwrap();
     }
 
     #[no_mangle]
