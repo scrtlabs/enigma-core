0edac4636b5d45d35adec7bf8039c5b8b1803d76
Elichai Turkel <elichai@enigma.co>
2019-03-05 19:57:09 +0200

    DEV: Fixed the secret contracts

--- examples/eng_wasm_contracts/erc20/src/lib.rs
+++ examples/eng_wasm_contracts/erc20/src/lib.rs
@@ -5,20 +5,18 @@
 
 extern crate eng_wasm;
 extern crate eng_wasm_derive;
-extern crate rustc_hex as hex;
 extern crate enigma_crypto;
-extern crate enigma_types;
 #[macro_use]
 extern crate serde;
+extern crate rustc_hex;
 
 use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
 use eng_wasm::String;
 use eng_wasm::from_utf8;
-use hex::ToHex;
 use std::collections::HashMap;
 use enigma_crypto::{KeyPair, hash::Keccak256};
-use enigma_types::UserAddress;
+use rustc_hex::ToHex;
 
 static TOTAL_SUPPLY: &str = "total_supply";
 
@@ -45,12 +43,24 @@ pub trait Erc20Interface{
 }
 
 /// User object holds all information of a user address
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct User {
     /// number of tokens a user owns
-    balance : u64,
+    balance : U256,
     /// a HashMap which contains all authorised spenders by the user
-    approved : HashMap<String, u64>,
+    approved : HashMap<String, U256>,
+}
+
+impl User {
+    pub fn get_approved(&self, addr: H256) -> U256 {
+        let addr: String = addr.to_hex();
+        *self.approved.get(&addr).unwrap_or(&Default::default())
+    }
+
+    pub fn insert_approved(&mut self, addr: H256, amount: U256) {
+        let addr: String = addr.to_hex();
+        self.approved.insert(addr, amount);
+    }
 }
 
 pub struct Contract;
@@ -59,23 +69,19 @@ impl Contract {
     /// receives an address and returns it's User object,
     /// if it does not exist, it creates a new object.
     fn get_user(user: H256) -> User {
-        match read_state!(&user.to_hex()) {
-            Some(user) => user,
-            // if does not exist, create a new user object
-            None => User { balance: 0, approved: HashMap::new() },
-        }
+        read_state!(&user.to_hex::<String>()).unwrap_or_default()
     }
 
     /// verify if the address that is sending the tokens is the one who actually sent the transfer.
     fn verify(from: H256, to: H256, amount: U256, sig: Vec<u8>) -> bool {
-        let mut msg = to.0.to_vec();
+        let mut msg = to.to_vec();
         msg.extend_from_slice(&amount.as_u64().to_be_bytes());
 
         let mut new_sig: [u8; 65] = [0u8; 65];
         new_sig.copy_from_slice(&sig[..65]);
 
         let accepted_pubkey = KeyPair::recover(&msg, new_sig).unwrap();
-        UserAddress::from(from.0) == accepted_pubkey.keccak256()
+        *from == *accepted_pubkey.keccak256()
     }
 }
 
@@ -83,38 +89,29 @@ impl Erc20Interface for Contract {
 
     #[no_mangle]
     fn mint(addr: H256, tokens: U256) {
-        let total_supply: u64 = match read_state!(TOTAL_SUPPLY) {
-            Some(amount) => amount,
-            None => 0,
-        };
+        let total_supply: U256 = read_state!(TOTAL_SUPPLY).unwrap_or_default();
         let mut user_addr = Self::get_user(addr);
 
         // update the user object and write to the state
-        user_addr.balance = user_addr.balance + tokens.as_u64();
-        write_state!(&addr.to_hex() => user_addr, TOTAL_SUPPLY => total_supply + tokens.as_u64());
+        user_addr.balance = user_addr.balance + tokens;
+        write_state!(&addr.to_hex::<String>() => user_addr, TOTAL_SUPPLY => total_supply + tokens);
     }
 
     #[no_mangle]
     fn total_supply() -> U256 {
-        match read_state!(TOTAL_SUPPLY) {
-            Some(amount) => amount,
-            None => 0,
-        }.into()
+        read_state!(TOTAL_SUPPLY).unwrap_or_default()
     }
 
     #[no_mangle]
     fn balance_of(token_owner: H256) -> U256 {
         let user: User = Self::get_user(token_owner);
-        user.balance.into()
+        user.balance
     }
 
     #[no_mangle]
     fn allowance(owner: H256, spender: H256) -> U256 {
         let user: User = Self::get_user(owner);
-        match user.approved.get(&spender.to_hex()) {
-            Some(amount) => *amount,
-            None => 0,
-        }.into()
+        user.get_approved(spender)
     }
 
     #[no_mangle]
@@ -123,24 +120,23 @@ impl Erc20Interface for Contract {
         let mut from_user : User = Self::get_user(from);
 
         // panic if the 'from' address does not have enough tokens.
-        assert!(from_user.balance >= tokens.as_u64(), "invalid action: user does not have enough tokens");
+        assert!(from_user.balance >= tokens, "invalid action: user does not have enough tokens");
         let mut to_user : User = Self::get_user(to);
 
         // update the balances and write the user objects to the state
-        from_user.balance = from_user.balance - tokens.as_u64();
-        to_user.balance = to_user.balance + tokens.as_u64();
-        write_state!(&from.to_hex() => from_user, &to.to_hex() => to_user);
+        from_user.balance = from_user.balance - tokens;
+        to_user.balance = to_user.balance + tokens;
+        write_state!(&from.to_hex::<String>() => from_user, &to.to_hex::<String>() => to_user);
     }
 
     #[no_mangle]
-    fn approve(token_owner: H256, spender: H256, tokens: U256, sig: Vec<u8>){
+    fn approve(token_owner: H256, spender: H256, tokens: U256, sig: Vec<u8>) {
         assert!(Self::verify(token_owner.clone(), spender.clone(), tokens, sig));
         let mut owner_user : User = Self::get_user(token_owner);
-        assert!(owner_user.balance >= tokens.as_u64(), "invalid action: owner does not have enough tokens");
-
+        assert!(owner_user.balance >= tokens, "invalid action: owner does not have enough tokens");
         // update the object and write it to the state
-        owner_user.approved.insert(spender.to_hex(), tokens.as_u64());
-        write_state!(&token_owner.to_hex() => owner_user);
+        owner_user.insert_approved(spender, tokens);
+        write_state!(&token_owner.to_hex::<String>() => owner_user);
     }
 
     #[no_mangle]
@@ -148,21 +144,18 @@ impl Erc20Interface for Contract {
         assert!(Self::verify(spender.clone(), to.clone(), tokens, sig));
         let mut owner_user : User = Self::get_user(owner);
         // panic if the owner does not own the amount of tokens
-        assert!(owner_user.balance >= tokens.as_u64(), "invalid action: owner does not have enough tokens");
+        assert!(owner_user.balance >= tokens, "invalid action: owner does not have enough tokens");
 
-        let allowed_balance: u64 = match owner_user.approved.get(&spender.to_hex()) {
-            Some(amount) => *amount,
-            None => 0,
-        };
+        let allowed_balance = owner_user.get_approved(spender);
         // panic if the spender is not approved to spend as much as tokens
-        assert!(allowed_balance >= tokens.as_u64(), "invalid action: user is not allowed to spend this amount of tokens");
+        assert!(allowed_balance >= tokens, "invalid action: user is not allowed to spend this amount of tokens");
 
         let mut to_user: User = Self::get_user(to);
 
         // update the objects and write to the state
-        to_user.balance = to_user.balance + tokens.as_u64();
-        owner_user.balance = owner_user.balance - tokens.as_u64();
-        owner_user.approved.insert(spender.to_hex(),allowed_balance - tokens.as_u64());
-        write_state!(&owner.to_hex() => owner_user, &to.to_hex() => to_user);
+        to_user.balance = to_user.balance + tokens;
+        owner_user.balance = owner_user.balance - tokens;
+        owner_user.insert_approved(spender, allowed_balance - tokens);
+        write_state!(&owner.to_hex::<String>() => owner_user, &to.to_hex::<String>() => to_user);
     }
 }
