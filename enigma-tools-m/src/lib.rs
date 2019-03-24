f061ee2299672c807e56a8d7d2be7d49db89fb11
Elichai Turkel <elichai@enigma.co>
2019-03-24 12:19:27 +0200

    DEV: Created a tools crate to be shared between trusted and untrusted sides

---
+++ enigma-tools-m/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
+#![deny(unused_extern_crates)]
+
+mod errors;
+pub mod keeper_types;
+pub mod primitives;
+
+#[cfg(feature = "std")]
+use {ethabi_std as ethabi, ethereum_types_std as ethereum_types, rmp_serde_std as rmp_serde, serde_json_std as serde_json,
+     serde_std as serde, std as localstd};
+
+#[cfg(feature = "sgx")]
+use {ethabi_sgx as ethabi, ethereum_types_sgx as ethereum_types, rmp_serde_sgx as rmp_serde, serde_json_sgx as serde_json,
+     serde_sgx as serde, sgx_tstd as localstd};
