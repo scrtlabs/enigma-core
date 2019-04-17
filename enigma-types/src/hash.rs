3ff8cf5bdfec9de256a3e23fa543b2f4c60209c4
Elichai Turkel <elichai@enigma.co>
2019-04-17 15:39:55 +0300

    DOC: Documented engima-types

--- enigma-types/src/hash.rs
+++ enigma-types/src/hash.rs
@@ -1,8 +1,14 @@
+//! # Hash Module
+//! This module provides a struct meant for containing Hashes (Kecack256 or Sha256).
+
 use core::ops::{Deref, DerefMut};
 use rustc_hex::{FromHex, FromHexError};
 use arrayvec::ArrayVec;
 use crate::serde::{Serialize, Deserialize};
 
+/// This struct is basically a wrapper over `[u8; 32]`, and is meant to be returned from whatever hashing functions we use.
+/// `#[repr(C)]` is a Rust feature which makes the struct be aligned just like C structs.
+/// See [`Repr(C)`][https://doc.rust-lang.org/nomicon/other-reprs.html]
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Default)]
 #[serde(crate = "crate::serde")]
 #[repr(C)]
@@ -10,10 +16,24 @@ pub struct Hash256([u8; 32]);
 
 
 impl Hash256 {
-    pub fn copy_from_slice(&mut self, src: &[u8]) {
+
+    /// This method exposes rust's built in [`std::slice::copy_from_slice`]
+    /// Copies the elements from `src` into `self`.
+    ///
+    /// The length of `src` must be the same as `self`.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the two slices have different lengths.
+    ///
+    /// This might not be needed since we implement `Deref` and `DerefMut` for the inner array.
+   pub fn copy_from_slice(&mut self, src: &[u8]) {
         self.0.copy_from_slice(src)
     }
 
+    /// This function converts a hex string into `Hash256` type.
+    /// the hex must not contain `0x` (as is usually the case in hexs in rust)
+    /// if the hex length isn't 64 (which will be converted into the 32 bytes) it will return an error.
     pub fn from_hex(hex: &str) -> Result<Self, FromHexError> {
         if hex.len() != 64 {
             return Err(FromHexError::InvalidHexLength);
@@ -24,6 +44,7 @@ impl Hash256 {
         Ok(result)
     }
 
+    /// Checks if the struct contains only zeroes or not.
     pub fn is_zero(&self) -> bool {
         self.0 == [0u8;32]
     }
--- enigma-types/src/lib.rs
+++ enigma-types/src/lib.rs
@@ -1,5 +1,16 @@
 #![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
-#![deny(unused_extern_crates)]
+#![deny(unused_extern_crates, missing_docs, warnings)]
+//! # Enigma Types
+//! This library is meant to supply all the types that are specific to our protocol. <br>
+//! Inside of this library I abstracted the std as `localstd` so that you can use it without knowing if it's `sgx_tstd` or regular std.
+//! *But* Unlike other crates this isn't just abstracting 2 different std's,
+//! but this crate is expected to work even without std at all(except some parts maybe).
+//!
+//! in the `build.rs` I use `cbindgen` to auto generate `enigma-types.h` header so it can be included into the edl.
+//! that way we can pass rust structs through the SGX bridge(which is C)
+//!
+//! This crate is Rust 2018 Edition,
+//! meaning there's no `extern crate` and `use` statements need to start with `crate`/`self`/`super`.
 
 
 pub mod traits;
@@ -15,6 +26,9 @@ use serde_std as serde;
 use crate::traits::SliceCPtr;
 pub use crate::types::*;
 
+/// This is a bit safer wrapper of [`core::ptr::copy_nonoverlapping`]
+/// it checks that the src len is at least as bigger as `count` otherwise it will panic.
+/// *and* it uses [`enigma_types::traits::SliceCPtr`] trait to pass a C compatible pointer.
 pub unsafe fn write_ptr<T>(src: &[T], dst: *mut T, count: usize) {
     if src.len() > count {
         unimplemented!()
