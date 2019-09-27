2f7d16bf0007057228be09407dc9ba0857772ef1
Moshe Demri <mdemri@gmail.com>
2019-09-27 09:50:39 +0300

    core run works

--- execute_core.sh
+++ execute_core.sh
@@ -5,4 +5,4 @@ LD_LIBRARY_PATH=/opt/intel/libsgx-enclave-common/aesm /opt/intel/libsgx-enclave-
 sleep 5 # give time to aesm_service to start
 
 cd /root/enigma-core/app
-. /opt/sgxsdk/environment && . /root/.cargo/env && RUST_BACKTRACE=1 ./target/debug/enigma-core-app -vv --debug-stdout
+. /opt/sgxsdk/environment && . /root/.cargo/env && RUST_BACKTRACE=1 ./target/debug/enigma-core-app -vv
