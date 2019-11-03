78279908888a9f2f3c26e4b8b39e385012324cc3
moria <moria@enigma.co>
2019-11-03 16:36:00 +0200

    DEV: changes according to the code review of PR#235

--- examples/eng_wasm_contracts/factorization/src/lib.rs
+++ examples/eng_wasm_contracts/factorization/src/lib.rs
@@ -7,19 +7,16 @@ struct S;
 #[pub_interface]
 impl S {
     pub fn find_number_of_prime_factors(mut number: u64) -> u64{
-        let mut result: u64 = 0;
-        let factor_bound = ((number as f64).sqrt().round() + 1.0) as u64;
-
         // The maximal number of 2s that divide `number`
-        while number & 1 == 0 {
-            result += 1;
-            number = number.clone() >> 1;
-        }
+        let mut result = number.trailing_zeros() as u64;
+        let factor_bound = ((number as f64).sqrt().ceil()) as u64;
+
+        number >>= result;
 
         for i in (3..factor_bound).step_by(2) {
             while number % i == 0 {
                 result += 1;
-                number = number.clone() / i;
+                number = number / i;
             }
         }
         result
