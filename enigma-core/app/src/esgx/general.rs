56f38c6b640e97d717cee20cd65c809a43e383c2
moria <moria@enigma.co>
2020-01-02 15:28:36 +0200

    DEV: improvement of initialization of km and core

--- enigma-core/app/src/esgx/general.rs
+++ enigma-core/app/src/esgx/general.rs
@@ -1,8 +1,7 @@
-use dirs;
 use enigma_tools_u::{self, esgx::general::storage_dir};
 use sgx_types::*;
 use sgx_urts::SgxEnclave;
-use std::{fs, path, io::Write};
+use std::{fs, path};
 use log;
 
 static ENCLAVE_FILE: &'static str = "../bin/enclave.signed.so";
@@ -11,52 +10,11 @@ pub static ENCLAVE_DIR: &'static str = ".enigma";
 
 #[logfn(INFO)]
 pub fn init_enclave_wrapper() -> SgxResult<SgxEnclave> {
-    // Step 1: try to retrieve the launch token saved by last transaction
-    //         if there is no token, then create a new one.
-    //
-    // try to get the token saved in $HOME */
-    //let mut home_dir = path::PathBuf::new();
-
-    let use_token = match dirs::home_dir() {
-        Some(path) => {
-            info!("Home dir is {}", path.display());
-            //home_dir = path;
-            true
-        }
-        None => {
-            error!("Cannot get home dir");
-            false
-        }
-    };
-
-    // Step : try to create a .enigma folder for storing all the files
-    // Create a directory, returns `io::Result<()>`
-    //let storage_path = home_dir.join(ENCLAVE_DIR);
+    // Create a storage folder for storage (Sealed, token, etc)
+    // If a storage folder is inaccessible, enclave cannot seal info
     let storage_path = storage_dir(ENCLAVE_DIR).unwrap();
-    match fs::create_dir(&storage_path) {
-        Err(why) => {
-            error!("Create .enigma folder => {:?}", why.kind());
-        }
-        Ok(_) => {
-            info!("Created new .enigma folder => {:?}", storage_path);
-        }
-    };
-    // Create the home/dir/.enigma folder for storage (Sealed, token , etc )
+    fs::create_dir_all(&storage_path).map_err(|e| { format_err!("Unable to create storage directory {}: {}", storage_path.display(), e) }).unwrap();
     let token_file: path::PathBuf = storage_path.join(ENCLAVE_TOKEN);
 
-    let (enclave, launch_token) = enigma_tools_u::esgx::init_enclave(&token_file, use_token, &ENCLAVE_FILE)?;
-    // Step 3: save the launch token if it is updated
-    if use_token && launch_token.is_some() {
-        // reopen the file with write capability
-        match fs::File::create(&token_file) {
-            Ok(mut f) => match f.write_all(&launch_token.unwrap()) {
-                Ok(_) => info!("Saved updated launch token!"),
-                Err(_) => error!("Failed to save updated launch token!"),
-            },
-            Err(_) => {
-                error!("Failed to save updated enclave token, but doesn't matter");
-            }
-        }
-    }
-    Ok(enclave)
+    enigma_tools_u::esgx::init_enclave(&token_file, &ENCLAVE_FILE)
 }
--- enigma-tools-u/src/esgx/general.rs
+++ enigma-tools-u/src/esgx/general.rs
@@ -1,52 +1,54 @@
 use sgx_types::{sgx_attributes_t, sgx_launch_token_t, sgx_misc_attribute_t, SgxResult};
 use sgx_urts::SgxEnclave;
-use std::env;
-use std::fs;
-use std::io::Read;
-use std::path::{self, PathBuf, Path};
-use dirs;
+use std::{fs, io::Read, path, io::Write};
+use std::path::{PathBuf, Path};
 use failure::Error;
 
 pub fn storage_dir<P: AsRef<Path>>(dir_name: P) -> Result<PathBuf, Error> {
-    let mut path = dirs::home_dir().ok_or_else(|| format_err!("Missing HomeDir"))?;
+    let mut path = dirs::home_dir().ok_or_else(|| {
+        format_err!("Missing home directory")
+    })?;
     trace!("Home dir is {}", path.display());
     path.push(dir_name);
     Ok(path)
 }
 
-pub fn init_enclave(token_path: &path::PathBuf, use_token: bool, enclave_location: &str)
-    -> SgxResult<(SgxEnclave, Option<sgx_launch_token_t>)> {
-    let path = env::current_dir().unwrap();
-    trace!("The current directory is {}", path.display());
+pub fn init_enclave(token_path: &path::PathBuf, enclave_location: &str)
+    -> SgxResult<(SgxEnclave)> {
     let mut launch_token: sgx_launch_token_t = [0; 1024];
     let mut launch_token_updated: i32 = 0;
 
-    if use_token {
-        match fs::File::open(&token_path) {
-            Err(_) => {
-                error!("Open token file {} error! Will create one.", token_path.as_path().to_str().unwrap());
+    match fs::File::open(&token_path) {
+        Err(e) => {
+            match e.kind() {
+                std::io::ErrorKind::NotFound => info!("Enclave launch token does not exist. Will create one."),
+                _ => warn!("Cannot open enclave launch token file: {}.", e)
             }
-            Ok(mut f) => {
-                info!("Open token file success! ");
-                match f.read(&mut launch_token) {
-                    Ok(1024) => {
-                        info!("Token file valid!");
-                    }
-                    _ => warn!("Token file invalid, will create new token file"),
-                }
+        },
+        Ok(mut f) => {
+            match f.read(&mut launch_token) {
+                Ok(1024) => info!("Enclave launch token found."),
+                _ => warn!("Enclave launch token invalid, will create a new one."),
             }
         }
     }
 
-    // Step 2: call sgx_create_enclave to initialize an enclave instance
+    // Call sgx_create_enclave to initialize an enclave instance
     // Debug Support: set 2nd parameter to 1
     let debug = 1;
     let mut misc_attr = sgx_misc_attribute_t { secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 }, misc_select: 0 };
     let enclave = SgxEnclave::create(enclave_location, debug, &mut launch_token, &mut launch_token_updated, &mut misc_attr)?;
 
     if launch_token_updated != 0 {
-        info!("Enclave created, Token: {:?}", enclave);
-        return Ok((enclave, Some(launch_token)));
+        // Save the launch token if it is updated
+        match fs::File::create(&token_path) {
+            Ok(mut f) => match f.write_all(&launch_token) {
+                Ok(_) => info!("Saved updated enclave launch token"),
+                Err(e) => warn!("Failed to save updated enclave launch token: {}", e),
+            },
+            Err(e) => warn!("Failed to save updated enclave launch token: {}", e)
+        }
+        info!("Enclave launch token was updated");
     }
-    Ok((enclave, None))
+    Ok(enclave)
 }
