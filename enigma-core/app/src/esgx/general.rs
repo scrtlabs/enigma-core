2760e074f126f501d593977ea817952e3defd4b6
moriaab <37746339+moriaab@users.noreply.github.com>
2020-01-02 23:37:16 +0200

    Update enigma-core/app/src/esgx/general.rs
    Co-Authored-By: Avishai Weingarten <33716232+AvishaiW@users.noreply.github.com>

--- enigma-core/app/src/esgx/general.rs
+++ enigma-core/app/src/esgx/general.rs
@@ -11,7 +11,7 @@ pub static ENCLAVE_DIR: &'static str = ".enigma";
 #[logfn(INFO)]
 pub fn init_enclave_wrapper() -> SgxResult<SgxEnclave> {
     // Create a folder for storage (Sealed, token, etc)
-    // If a storage folder is inaccessible, enclave cannot seal info
+    // If the storage folder is inaccessible, the enclave would not be able to seal info
     let storage_path = storage_dir(ENCLAVE_DIR).unwrap();
     fs::create_dir_all(&storage_path).map_err(|e| { format_err!("Unable to create storage directory {}: {}", storage_path.display(), e) }).unwrap();
     let token_file: path::PathBuf = storage_path.join(ENCLAVE_TOKEN);
