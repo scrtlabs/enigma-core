1191e75ba612da2331519285bf451b24f3c30277
moriaab <37746339+moriaab@users.noreply.github.com>
2020-01-02 23:39:10 +0200

    Update enigma-principal/app/src/esgx/general.rs
    Co-Authored-By: Avishai Weingarten <33716232+AvishaiW@users.noreply.github.com>

--- enigma-principal/app/src/esgx/general.rs
+++ enigma-principal/app/src/esgx/general.rs
@@ -17,7 +17,7 @@ pub fn init_enclave_wrapper() -> SgxResult<SgxEnclave> {
     let storage_path = storage_dir(ENCLAVE_DIR).unwrap();
     fs::create_dir_all(&storage_path).map_err(|e| { format_err!("Unable to create the storage directory {}: {}", storage_path.display(), e) }).unwrap();
     let epoch_storage_path = storage_path.join(EPOCH_DIR);
-    fs::create_dir_all(&epoch_storage_path).map_err(|e| { format_err!("Unable to create epoch storage directory {}: {}", epoch_storage_path.display(), e) }).unwrap();
+    fs::create_dir_all(&epoch_storage_path).map_err(|e| { format_err!("Unable to create the epoch storage directory {}: {}", epoch_storage_path.display(), e) }).unwrap();
     let state_storage_path = storage_path.join(STATE_KEYS_DIR);
     fs::create_dir_all(&state_storage_path).map_err(|e| { format_err!("Unable to create state storage directory {}: {}", state_storage_path.display(), e) }).unwrap();
 
